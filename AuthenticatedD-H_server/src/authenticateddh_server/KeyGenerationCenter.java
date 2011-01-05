9089bbc5d79003c276ce4e73e42ee2eb9e558aa6
R.Malinowski88@gmail.com <R.Malinowski88@gmail.com@a977f4fc-478d-022e-a20c-af48b3c59885>
2011-01-05 00:05:55 +0000

    KGC rośnie w siłę :)

--- AuthenticatedD-H_server/src/authenticateddh_server/KeyGenerationCenter.java
+++ AuthenticatedD-H_server/src/authenticateddh_server/KeyGenerationCenter.java
@@ -2,11 +2,15 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package authenticateddh_server;
 
 import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,42 +27,49 @@ public class KeyGenerationCenter {
     private static final BigInteger ZERO = new BigInteger(String.valueOf(0));
 
     /* Konstruktor domyslny */
-    private KeyGenerationCenter(){
-        groupLength_=128;
+    private KeyGenerationCenter() {
+        groupLength_ = 128;
         inititializeKGC();
         // na razie nie ma tu nic sensownego
     }
 
-    private boolean inititializeKGC(){
-        SecureRandom random = new SecureRandom();
-        primeOrder_ = BigInteger.probablePrime(groupLength_, random);
+    private boolean inititializeKGC() {
 
-        generator_=getRandomGenerator();
-        System.out.println("grupa to " + primeOrder_ + ", natomiast generator to " + generator_);
+        GeneratorFactory fact = new GeneratorFactory(96);
+        primeOrder_ = fact.getP();
+        generator_ = fact.getG();
+        System.out.println("grupa to " + primeOrder_ + ",jej dlugosc to " + primeOrder_.bitLength() + ", natomiast generator to " + generator_ + ",a jej dlugosc to " + generator_.bitLength());
         return false;
+    }
+
+    static private int H1(BigInteger a) {
+        try {
+            byte[] defaultBytes = a.toByteArray();
+            MessageDigest algorithm = MessageDigest.getInstance("MD5");
+            algorithm.reset();
+            algorithm.update(defaultBytes);
+            byte messageDigest[] = algorithm.digest();
+            //System.out.println();
+            BigInteger result = new BigInteger(messageDigest);
+            System.out.println("liczba po wyjsciu z messageDigest to " + result + ", natomiast jej dlugosc to " + result.bitLength());
+            System.out.printf("p = %x (%d bits)\n", result, result.bitLength());
+        } catch (NoSuchAlgorithmException ex) {
+            //Logger.getLogger(KeyGenerationCenter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 9;
 
     }
 
-
-    private BigInteger getRandomGenerator(){
+    public static void main(String[] args) {
+        System.out.println("Testujemy dzialanie tego syfu");
+        KeyGenerationCenter KGC = new KeyGenerationCenter();
+        double gener = 4;
+        double order = 7;
         SecureRandom random = new SecureRandom();
-        BigInteger tempGenerator;
-        do{
-            tempGenerator = new BigInteger(groupLength_, random);
-            System.out.println("dzialam");
-        }while (!numberIsGenerator(tempGenerator, primeOrder_) || (tempGenerator.compareTo(primeOrder_) > -1) || (tempGenerator.equals(ZERO)) );
-        return tempGenerator;
+        BigInteger a = new BigInteger(1038283, random);
+        H1(a);
+        H1(a);
+        //if (isIntGenerator(gener, order)) System.out.println(gener +" jest generatorem dla order rownego " + order);
+        //else System.out.println(gener +" nie jest generatorem dla order rownego " + order);
     }
-
-    private boolean numberIsGenerator(BigInteger number, BigInteger order){
-        BigInteger gcd = number.gcd(order);
-        if (gcd.equals(ONE) && !number.equals(ONE)) return true;
-        else return false;
-    }
-
-
-     public static void main(String[] args) {
-     System.out.println("Testujemy dzialanie tego syfu");
-     KeyGenerationCenter KGC = new KeyGenerationCenter();
-     }
 }
